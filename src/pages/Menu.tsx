cb15213e15fcac6085eccb947d2ed44b6edc1489
Japowww <salesjaspherjules@gmail.com>
2026-04-07 08:58:52 +0800

    Adding About page in this Activity

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,6 +1,11 @@
 import {
+  IonContent,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
   IonMenu,
+  IonMenuToggle,
   IonPage,
   IonRouterOutlet,
   IonSplitPane,
@@ -9,8 +14,14 @@ import {
 } from "@ionic/react";
 import { Redirect, Route } from "react-router";
 import Home from "./Home";
+import About from "./About";
+import { homeOutline } from "ionicons/icons";
 
 const Menu: React.FC = () => {
+  const path = [
+    { name: "Home", url: "/app/home", icon: homeOutline },
+    { name: "About", path: "/app/about" },
+  ];
   return (
     <IonPage>
       <IonSplitPane contentId="main">
@@ -20,12 +31,24 @@ const Menu: React.FC = () => {
               <IonTitle>Menu</IonTitle>
             </IonToolbar>
           </IonHeader>
+          <IonContent>
+            {path.map((item, index) => (
+              <IonMenuToggle key={index}>
+                <IonItem routerLink={item.url} routerDirection="forward">
+                  <IonIcon icon={item.icon} slot="start"></IonIcon>
+                  {item.name}
+                </IonItem>
+              </IonMenuToggle>
+            ))}
+          </IonContent>
         </IonMenu>
         <IonRouterOutlet id="main">
           <Route exact path="/app/home" component={Home} />
           <Route exact path="/app">
             <Redirect to="/app/home" />
           </Route>
+
+          <Route exact path="/app/home/About" component={About} />
         </IonRouterOutlet>
       </IonSplitPane>
     </IonPage>
